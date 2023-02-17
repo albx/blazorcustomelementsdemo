db363301965ba991098a943d83b70d5d2d18ee8e
Alberto Mori <a.mori@abletech.it>
2023-02-17 14:28:48 +0100

    mvc website stub

---
+++ src/MyAwesomeBlog.Web.Site/Controllers/PostsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAwesomeBlog.Web.Site.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult Archive()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(string slug)
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MyAwesomeBlog.Web.Site/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyAwesomeBlog.Web.Site.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
