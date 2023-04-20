2e8c37d43ee384fa737f98a9f168c6b6a03ce568
Alberto Mori <albi.mo87@hotmail.it>
2023-04-20 18:05:16 +0200

    added reference to customelements package

--- src/MyAwesomeBlog.Web.UI.Blazor/Program.cs
+++ src/MyAwesomeBlog.Web.UI.Blazor/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyAwesomeBlog.Web.UI.Blazor;
+using MyAwesomeBlog.Web.UI.Blazor.Components;
 using MyAwesomeBlog.Web.UI.Blazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.RootComponents.RegisterCustomElement<PostComments>("post-comments");
 
 builder.Services.AddHttpClient<CommentsService>(client => client.BaseAddress = new Uri("http://localhost:5238"));
 
