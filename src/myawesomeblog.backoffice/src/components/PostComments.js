a596f6e3ecf384293d8c3e191314cc85ae7e7abb
Alberto Mori <albi.mo87@hotmail.it>
2023-04-25 09:19:22 +0200

    event callback via javascript

--- src/myawesomeblog.backoffice/src/components/PostComments.js
+++ src/myawesomeblog.backoffice/src/components/PostComments.js
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 const buildPostDetailUrl = (id, slug) => `/post/${id}/${slug}`;
 
 function PostComments() {
     const { id, slug } = useParams();
+
+    useEffect(() => {
+        const registerBlazorStart = () => {
+            window.Blazor.start().then(() => {
+                const postComments = document.querySelector("post-comments");
+                const postId = postComments.getAttribute('post-id');
+        
+                postComments.title = 'Hello from backoffice';
+                postComments.onCommentAdded = async (comment) => {
+                    try {
+                        const url = `http://localhost:5238/api/posts/${postId}/comments`;
+                        await axios.post(url, comment);
+
+                        alert('Comment added successfully!');
+                    } catch (error) {
+                        alert ('Error adding a comment');
+                    }
+                }
+            })
+        }
+
+        registerBlazorStart()
+    }, []);
 
     return (
         <>
