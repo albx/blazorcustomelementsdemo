8e9023af7f13e15cc819a3d0e8edfc9c39f82909
Alberto Mori <albi.mo87@hotmail.it>
2023-05-09 21:50:22 +0200

    deploy script

---
+++ src/MyAwesomeBlog.Web.UI.Blazor/deploy.ps1
@@ -0,0 +1,4 @@
+dotnet publish .\MyAwesomeBlog.Web.UI.Blazor.csproj -c Release -o .\bin\Release\published
+
+Copy-Item -Path ".\bin\Release\published\wwwroot\_framework" -Destination "..\myawesomeblog.backoffice\public\_framework" -Recurse
+Copy-Item -Path ".\bin\Release\published\wwwroot\_content" -Destination "..\myawesomeblog.backoffice\public\_content" -Recurse 
