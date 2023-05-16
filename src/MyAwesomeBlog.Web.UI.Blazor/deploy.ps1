2bd30f39057ac6100dc4064ec817cc784122b544
Alberto Mori <albi.mo87@hotmail.it>
2023-05-16 23:59:40 +0200

    add css isolation

--- src/MyAwesomeBlog.Web.UI.Blazor/deploy.ps1
+++ src/MyAwesomeBlog.Web.UI.Blazor/deploy.ps1
@@ -1,4 +1,5 @@
 dotnet publish .\MyAwesomeBlog.Web.UI.Blazor.csproj -c Release -o .\bin\Release\published
 
 Copy-Item -Path ".\bin\Release\published\wwwroot\_framework" -Destination "..\myawesomeblog.backoffice\public\_framework" -Recurse
-Copy-Item -Path ".\bin\Release\published\wwwroot\_content" -Destination "..\myawesomeblog.backoffice\public\_content" -Recurse 
+Copy-Item -Path ".\bin\Release\published\wwwroot\_content" -Destination "..\myawesomeblog.backoffice\public\_content" -Recurse
+Copy-Item -Path ".\bin\Release\published\wwwroot\MyAwesomeBlog.Web.UI.Blazor.styles.css" -Destination "..\myawesomeblog.backoffice\public"
